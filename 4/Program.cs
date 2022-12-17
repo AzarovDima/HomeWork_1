3f90c04493686deb4d1163867abb06fb36851d06
AzarovDima <ada89885308140@gmail.com>
2022-12-17 23:32:15 +0300

    4 задача начало

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите прорамму, которая на вход принимает число N а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите целоче число: ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+int sum = 2;
+int result = 0;
+
+difference = number - 0;
+result = number % 2;
+
+while
+{
+
+}
+if (number >= 2)
+{
+
+}
