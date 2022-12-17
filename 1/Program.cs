8ba3f46956c6f14a3bdbe2035ce17059058928f9
AzarovDima <ada89885308140@gmail.com>
2022-12-17 21:41:30 +0300

    Первая задача

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая на вход принимает два числа и выдает, какое число больше а какое меньше
+// Например а = 5; b = 7 -> MAX = 7
+
+Console.Write("Введите ПЕРВОЕ целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+
+Console.Write("Введите ВТОРОЕ целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int number2 = int.Parse(userInput2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} ---> Максимальное!!!");
+}
+else
+{
+    Console.WriteLine($"Число {number2} ---> Максимальное!!!");
+}
+// КОНЕЦ!!!
