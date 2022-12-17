f7e9d69b3bedfb653afbd7eba68fb039d3e46f53
AzarovDima <ada89885308140@gmail.com>
2022-12-17 23:08:14 +0300

    3 задача

---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+// ПРИМЕР 4 делиться, 7 не делиться!
+
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+
+int result = number % 2;
+
+if (result == 0)
+{
+    Console.WriteLine($"Число {number} делится на два без остатка");
+}
+else
+{
+    Console.WriteLine($"Число {number} не делится на два без остатка");
+}
+
